ee268fba8f56e3d6e85bf172bfbc8e1bc766113d
Max <mrc550email@gmail.com>
2025-05-19 09:24:29 -0500

    Update main.py
    Bigger brush and can delete sand with right click. Also made it update in one array

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import sys
 import copy
 
 # Modify these
-width = 250 # columns
-height = 250 # rows
+width = 80 # columns
+height = 80 # rows
 
 # Set cell and window size
-cell_size = 3 # Size of each square cell in pixels
+cell_size = 10 # Size of each square cell in pixels
 window_width = width * cell_size
 window_height = height * cell_size
 
 #
-drop_size = 20
+drop_size = 2
+drop_size *=5
 
 closest_zero_y = None
 
@@ -34,7 +35,7 @@ def draw_grid(screen):
             # Optional: draw cell borders
             # pygame.draw.rect(screen, (40, 40, 40), rect, 1)
 
-def update():
+"""def update():
     prev_grid = [row[:] for row in grid]
     for y in range(height - 2, -1, -1):  # from second-to-last row up to 0, going upwards
         for x in range(width):
@@ -55,6 +56,29 @@ def update():
                         grid[y+1][x-1] = 1
                     elif x < width - 1 and prev_grid[y+1][x+1] == 0:
                         grid[y][x] = 0
+                        grid[y+1][x+1] = 1"""
+
+def update():
+    prev_grid = [row[:] for row in grid]
+    for y in range(height - 2, -1, -1):  # from second-to-last row up to 0, going upwards
+        for x in range(width):
+            if grid[y][x] == 1 and grid[y+1][x] == 0:
+                grid[y][x] = 0
+                grid[y+1][x] = 1
+            else:
+                if grid[y][x] == 1:
+                    closest_zero_y = None
+                    for check_y in range(y+1, height):
+                        if grid[check_y][x] == 0:
+                            closest_zero_y = check_y
+                            break
+                    if closest_zero_y is not None:
+                        break
+                    elif x > 0 and grid[y+1][x-1] == 0:
+                        grid[y][x] = 0
+                        grid[y+1][x-1] = 1
+                    elif x < width - 1 and grid[y+1][x+1] == 0:
+                        grid[y][x] = 0
                         grid[y+1][x+1] = 1
 
 
@@ -62,14 +86,16 @@ def mouse_drag():
     pressed = pygame.mouse.get_pressed()
     if pressed[0]:  # Left mouse button is held
         x, y = pygame.mouse.get_pos()
-        grid[int(y / cell_size)][int(x / cell_size)] = 1
-        """for a in range(drop_size):
+        # grid[int(y / cell_size)][int(x / cell_size)] = 1
+        for a in range(drop_size):
             for b in range(drop_size):
-                print(a)
-                if int((y + a - 0.5*drop_size) / cell_size) % 2 == 0 and int((x + b - 0.5*drop_size) / cell_size) % 2 ==0:
-
-                    print("even")
-                    grid[int((y + a - 0.5*drop_size) / cell_size)][int((x + b - 0.5*drop_size) / cell_size)] = 1"""
+                    grid[int((y + a - drop_size) / cell_size)][int((x + b - drop_size) / cell_size)] = 1       
+    if pressed[2]:  # Left mouse button is held
+        x, y = pygame.mouse.get_pos()
+        # grid[int(y / cell_size)][int(x / cell_size)] = 1
+        for a in range(drop_size):
+            for b in range(drop_size):
+                    grid[int((y + a - drop_size) / cell_size)][int((x + b - drop_size) / cell_size)] = 0        
 
 pygame.init()
 screen = pygame.display.set_mode((window_width, window_height))
@@ -88,7 +114,7 @@ while running:
     draw_grid(screen)
     pygame.display.flip()
 
-    clock.tick(40) # Slowed down for easier viewing
+    clock.tick(60) # Slowed down for easier viewing
 
     update()
     mouse_drag()
